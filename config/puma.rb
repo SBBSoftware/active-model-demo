68cd50199e2c6cee3d42822e3f2ca3948d101a05
spc9999 <sean.chatterton@sbbsoftware.com>
2015-12-14 23:56:00 -0500

    adding puma

--- config/puma.rb
+++ config/puma.rb
@@ -6,14 +6,14 @@ require 'puma'
 threads 1, 6
 
 app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
+shared_dir = "#{app_dir}/puma"
 
 # Default to production
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 # Set up socket location
-bind "unix://#{shared_dir}/sockets/puma.sock"
+bind "unix://#{shared_dir}/sockets/sbb.sock"
 
 # Logging
 stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
