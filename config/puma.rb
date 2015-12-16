527b3c9f85095e4f1ad774e38d03d1371d513629
spc9999 <sean.chatterton@sbbsoftware.com>
2015-12-16 13:05:09 -0500

    adding puma

--- config/puma.rb
+++ config/puma.rb
@@ -5,30 +5,31 @@ workers 2
 # Min and Max threads per worker
 threads 1, 6
 # for mina
-app_dir = File.expand_path("../../../../", __FILE__)
-shared_dir = "#{app_dir}/puma"
+app_dir = File.expand_path("../../../", __FILE__)
+t = File.expand_path("../../../../", __FILE__)
+puma_dir = "#{t}/puma"
 
 # Default to production
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 # Set up socket location
-socket_location = "unix://#{shared_dir}/sockets/sockets.sbb.sock"
+socket_location = "unix://#{puma_dir}/sockets/sockets.sbb.sock"
 bind socket_location
 puts '-------------------------'
 puts 'Enviornment settings'
 puts "App dir #{app_dir}"
-puts "Shared dir #{shared_dir}"
+puts "Shared dir #{puma_dir}"
 puts "Binding to socket #{socket_location}"
 puts '-------------------------'
 puts 'listening'
 # Logging
 
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+stdout_redirect "#{puma_dir}/log/puma.stdout.log", "#{puma_dir}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
+pidfile "#{puma_dir}/pids/puma.pid"
+state_path "#{puma_dir}/pids/puma.state"
 activate_control_app
 
 on_worker_boot do
